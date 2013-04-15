aa8e54baf1117ebdb5bd6f19bbda39d43835501b
Roger rowe <rrowe1@umbc.edu>
2013-04-15 00:42:48 +0000

    Chapter 9 exercise 3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -44,6 +44,14 @@ describe "Authentication" do
 			end
 		end
 
+		describe "for non-signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			it {should_not have_link('Users', href: users_path)}
+			it {should_not have_link('Profile', href: user_path(user)) }
+			it {should_not have_link('Settings', href: edit_user_path(user)) }
+			it {should_not have_link('Sign out', href: signout_path) }
+		end			
 	end
 
 	describe "authorization" do
