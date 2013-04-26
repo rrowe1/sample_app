bb3ea9243db30913ddeca35524ff1d850b30855d
Roger rowe <rrowe1@umbc.edu>
2013-04-26 22:20:48 +0000

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,9 +34,36 @@ describe "Home page" do
 		    page.should have_selector("li##{item.id}", text: item.content)
 		  end
 		end
-
 		
 	end
+
+	describe "for signed-in users" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			FactoryGirl.create(:micropost, user: user, content: "Lorem")
+			FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+			sign_in user
+			visit root_path
+		end
+
+		it "should render the user's feed" do
+			user.feed.each do |item|
+				page.should have_selector("li##{item.id}", text: item.content)
+			end
+		end
+
+		describe "follower/following counts" do
+			let(:other_user) { FactoryGirl.create(:user) }
+			before do
+				other_user.follow!(user)
+				visit root_path
+			end
+			
+			it { should have_link("0 following", href: following_user_path(user)) }
+			it { should have_link("1 followers", href: followers_user_path(user)) }
+		end
+	end
+
 end
 
 describe "Help page" do
@@ -69,18 +96,19 @@ describe "Contact page" do
 
 end
 
-	it "should have the right links on the layout" do
-		visit root_path
-		click_link "About"
-		page.should have_selector 'title', text: full_title('About Us')
-		click_link "Help"
-		page.should have_selector 'title', text: full_title('Help')
-		click_link "Contact"
-		page.should have_selector 'title', text: full_title('Contact')
-		click_link "Home"
-		click_link "Sign up now!"
-		page.should have_selector 'title', text: full_title('Sign up')
-		click_link "sample app"
-		page.should have_selector 'title', text: full_title('')
-	end
+it "should have the right links on the layout" do
+	visit root_path
+	click_link "About"
+	page.should have_selector 'title', text: full_title('About Us')
+	click_link "Help"
+	page.should have_selector 'title', text: full_title('Help')
+	click_link "Contact"
+	page.should have_selector 'title', text: full_title('Contact')
+	click_link "Home"
+	click_link "Sign up now!"
+	page.should have_selector 'title', text: full_title('Sign up')
+	click_link "sample app"
+	page.should have_selector 'title', text: full_title('')
+end
+
 end
